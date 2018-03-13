3766916a05a3e6ed846d3ee95fb47a99c30ba2ba
telefoon13 <Mike0528>
2018-03-13 16:12:45 +0100

    Add project files.

---
+++ MVC_Security/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_Security.Startup))]
+namespace MVC_Security
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
